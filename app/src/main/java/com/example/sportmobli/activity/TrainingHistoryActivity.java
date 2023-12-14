8ec0fb86fa0a539dc4f50d9ba96429dc3eca8a13
kgypeter <kgyulapeter10@gmail.com>
2023-12-14 17:32:47 +0200

    - Home: The two buttons which redirect to the diet and training history pg
    - DietHistoryActivity: Recyclerview for
    the list diet history.
    
    - TrainingHistoryActivity:Recyclerview
    for the list diet history.
    
    - layout for the history items

---
+++ app/src/main/java/com/example/sportmobli/activity/TrainingHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.sportmobli.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sportmobli.R;
+
+public class TrainingHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_history);
+
+    }
+}
